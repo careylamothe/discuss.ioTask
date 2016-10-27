491534a382c66b568507818dca9dd611cee624d9
Carey LaMothe <carey.lamothe@gmail.com>
2016-10-26 18:08:41 -0700

    added functions

--- index.js
+++ index.js
@@ -1,8 +1,49 @@
 (function() {
+
+  function newQuestion() {
+    $("#add-edit").slideDown("slow");
+  }
+
+  function cancelAddEdit() {
+      $("#add-edit").slideUp();
+      $("#new-text").val(" ");
+  }
+
+  function addListItem() {
+    var text = $("#new-text").val();
+
+    $("#questions-list").append('<li class="list-group-item">' +
+    '<span class="badge pull-left col-xs-1">?</span>' + text +
+    '<span><a href="#" class="col-xs-offset-7">Edit</a></span>' +
+    '<span class="delete glyphicon glyphicon-trash pull-right" aria-hidden="true"><a href="#"></a></span></li>');
+    $("#add-edit").slideUp();
+    $("#new-text").val(" ");
+  }
+
+  function removeListItem() {
+    $(this).parent().remove();
+  }
+
+  function editQuestion() {
+    newQuestion();
+    //get KU
+  }
+
   $(document).ready(function() {
       // $('div').fadeIn('slow');
-      $("#panel").click(function() {
-        $("#add-edit").slideDown("slow");
-      })
+      $("#add").on('click', addListItem);
+
+      $(".delete").on('click', removeListItem);
+
+      $(document).on('click', '.delete', removeListItem);
+
+      $("#new-question").on('click', newQuestion);
+
+      $(".edit").on('click'), editQuestion);
+
+      $("#cancel").on('click', cancelAddEdit);
+
+      // $("#questions-list").sortable();
+      // $("#questions-list").disableSelection();
   });
 })();
